a219074b77627da27911cd45e8af3f91111cec29
hannesmalm <128385053+hannesmalm@users.noreply.github.com>
2023-12-13 15:10:13 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MittClick.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
