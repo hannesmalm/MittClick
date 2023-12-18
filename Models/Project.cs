6e44dcff6e4ff9ceb386201c1d6bcf3d4f51513b
hannesmalm <128385053+hannesmalm@users.noreply.github.com>
2023-12-19 00:02:02 +0100

    Lade till klass + uppdaterade datatyp

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,6 +1,19 @@
-﻿namespace MittClick.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MittClick.Models
 {
     public class Project
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public int ProjectLeader {  get; set; }
+        [ForeignKey(nameof(ProjectLeader))]
+        public virtual User? User { get; set; }
+
     }
 }
