6fdd84f4587663e33b9f18bcb0cd19ebefcd9b28
Phizzledizzle <phialv231@edunet.oru.se>
2023-12-21 12:51:47 +0100

    ViewModels för Login + Register. Loginsida "färdig"(utan funktion)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,23 @@
-﻿namespace MittClick.Controllers
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MittClick.Models;
+
+namespace MittClick.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            return View(loginViewModel);
+        }
+
+        public IActionResult Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
     }
 }
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MittClick.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Skriv ditt användarnamn.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Skriv ditt lösenord.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
