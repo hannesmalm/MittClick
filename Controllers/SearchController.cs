86715568f76c8e1c9e46d9bfc4a142a091b4fe14
hannesmalm <128385053+hannesmalm@users.noreply.github.com>
2024-01-03 15:25:34 +0100

    + sök

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language;
+using MittClick.Models;
+
+namespace MittClick.Controllers
+{
+    public class SearchController : Controller
+    {
+        private MittClickDbContext profiles;
+        public SearchController(MittClickDbContext service)
+        {
+            profiles = service;
+        }
+        public IActionResult Result()
+        {
+            var profileList = from profile in profiles.Profiles
+                           select profile;
+            return View(profileList.ToList());
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
