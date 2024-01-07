0e2e4a126cf58593a2e5d9d7205d4ccbc63cb49b
lillyjuliat <testonglilly@gmail.com>
2024-01-07 13:32:31 +0100

    försk till merge

--- Models/MittClickDbContext.cs
+++ Models/MittClickDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace MittClick.Models
@@ -9,15 +10,38 @@ namespace MittClick.Models
 
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Project> Projects { get; set; }
-        public DbSet<Skill>Skills { get; set; }
-        public DbSet<ContactInfo> Contacts { get; set; }
-        public DbSet<Image>Images { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<PartOfProject> PartOfProjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PartOfProject>()
+                .HasKey(pp => new { pp.UId, pp.PId });
+
+            modelBuilder.Entity<PartOfProject>()
+                .HasOne(pp => pp.User)
+                .WithMany(u => u.PartOfProjects)
+                .HasForeignKey(pp => pp.UId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<PartOfProject>()
+                .HasOne(pp => pp.Project)
+                .WithMany(p => p.PartOfProjects)
+                .HasForeignKey(pp => pp.PId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(p => p.UserId);
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-
-            optionsBuilder.UseLazyLoadingProxies();
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("ApplicationDbContext");
+            }
         }
     }
 }
