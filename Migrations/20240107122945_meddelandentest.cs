a3c06e15049ff8b7208b6076bfe50c2e7cec89be
Phizzledizzle <phialv231@edunet.oru.se>
2024-01-08 00:28:36 +0100

    Revert "Profilbild i navbar, snyggare send + validering, snyggare inbox"
    This reverts commit 9d5c8f906be8a4c06c68d0607086335ae746ca1b.

---
+++ Migrations/20240107122945_meddelandentest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MittClick.Migrations
+{
+    /// <inheritdoc />
+    public partial class meddelandentest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SenderId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiverName",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderName",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReceiverName",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "SenderName",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SenderId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
