21c6f1280fd66463cdfe6f9362ecaa73fb26d510
Phizzledizzle <phialv231@edunet.oru.se>
2024-01-22 16:59:47 +0100

    listor i profil = jettefin

--- Models/ViewModels/UpdateWorkExperienceViewModel.cs
+++ Models/ViewModels/UpdateWorkExperienceViewModel.cs
@@ -10,12 +10,11 @@ namespace MittClick.Models.ViewModels
 
 		[Required]
 		public string Role { get; set; }
+
 		[Required]
-		[Range(1900, 2099)]
 		public int From { get; set; }
 
-		[Range(1900, 2099)]
-		public int To { get; set; }
+		public int? To { get; set; }
 
 		[Required]
         public List<WorkExperience> WorkExperiences {  get; set; }
