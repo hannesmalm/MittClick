809a96259ed22cf2e901115e953947526872468e
hannesmalm <128385053+hannesmalm@users.noreply.github.com>
2024-01-22 17:21:28 +0100

    + contact fungerar

--- Models/ViewModels/UpdateContactInfoViewModel.cs
+++ Models/ViewModels/UpdateContactInfoViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace MittClick.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MittClick.Models.ViewModels
 {
     public class UpdateContactInfoViewModel
     {
         public List<ContactInfo> ContactInfos { get; set; }
         public Profile Profile { get; set; }
+        [Required]
+        public string Info { get; set; }
+        [Required]
+        public string Type { get; set; }
+
     }
 }
